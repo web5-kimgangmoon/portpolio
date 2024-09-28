b22176bc524c44db52e1268b4121cb44f81d8913
kimgangmoon <162942474+web5-kimgangmoon@users.noreply.github.com>
2024-09-28 12:09:44 +0900

    feat:img change

--- portpolio/app/layout.tsx
+++ portpolio/app/layout.tsx
@@ -3,10 +3,14 @@ import type { Metadata } from "next";
 // import Inter from "next/font/google";
 import localfont from "next/font/local";
 import "@/public/globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "portpolio kim",
   description: "just portpolio",
+  icons: {
+    icon: "/project.png",
+  },
 };
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +23,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <meta property="og:image" content="/project.png" />
+        <meta property="og:title" content="portpolio" />
+        <meta
+          property="og:description"
+          content="자꾸 이미지 링크가 얼굴 사진이 떠서 수정합니다"
+        />
+      </head>
       <body className={myFont.className}>{children}</body>
     </html>
   );
