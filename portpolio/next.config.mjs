a5a2a1b8a0fe6076c66739a84711a95e66de4411
kimgangmoon <162942474+web5-kimgangmoon@users.noreply.github.com>
2025-01-18 02:01:02 +0900

    chore:nextConfig

--- portpolio/next.config.mjs
+++ portpolio/next.config.mjs
@@ -1,32 +1,34 @@
-import withPlugins from "next-compose-plugins";
-import withImages from "next-images";
-
-/** @type {import('next').NextConfig} */
-const nextConfig = withPlugins([
-  [
-    withImages,
-    {
-      unoptimized: true,
-      assetPrefix: "/portpolio/",
-    },
-  ],
-  {
-    trailingSlash: true,
-    basePath: "/portpolio",
-    assetPrefix: "/portpolio/",
-  },
-]);
-
-export default nextConfig;
+// import withPlugins from "next-compose-plugins";
+// import withImages from "next-images";
 
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   basePath: "/portpolio",
-//   output: "export",
-//   assetPrefix: "/portpolio/",
-//   images: {
-//     unoptimized: true,
+// const nextConfig = withPlugins([
+//   [
+//     withImages,
+//     {
+//       unoptimized: true,
+//       assetPrefix: "/portpolio/",
+//     },
+//   ],
+//   {
+//     trailingSlash: true,
+//     basePath: "/portpolio",
+//     assetPrefix: "/portpolio/",
 //   },
-// };
+// ]);
 
 // export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/portpolio",
+  output: "export",
+  assetPrefix: "/portpolio/",
+  images: {
+    unoptimized: true,
+    assetPrefix: "/portpolio/",
+  },
+  trailingSlash: true,
+};
+
+export default nextConfig;
